3d237aa35c735fd16972b20bb6423ddef1bb75b1
Yaroslav Ovdii <yaovdiy@gmail.com>
2025-01-01 01:04:25 +0200

    fonts: added selected fonts for use

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1 class="header text-4xl">Welcome to SvelteKit</h1>
+<p class="prose-bold">Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
