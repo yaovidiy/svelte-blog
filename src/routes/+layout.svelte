15851a2c7d563fe4c31230d932ffa83621874047
Yaroslav Ovdii <yaovdiy@gmail.com>
2025-01-01 00:37:51 +0200

    ui: added theme switcher and base ui components

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
-	import { i18n } from "$lib/i18n";
-	import { ParaglideJS } from "@inlang/paraglide-sveltekit";
+	import { ModeWatcher } from 'mode-watcher';
+	import ThemeToggle from '$lib/components/app/theme-toggle/ThemeToggle.svelte';
+	import { i18n } from '$lib/i18n';
+	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
 	import '../app.css';
 	let { children } = $props();
 </script>
 
 <ParaglideJS {i18n}>
+	<ModeWatcher />
+	<ThemeToggle />
 	{@render children()}
 </ParaglideJS>
