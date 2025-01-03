73e1458d4c7992262af6abe95258f319ebe3e05b
Yaroslav Ovdii <yaovdiy@gmail.com>
2025-01-03 17:19:05 +0200

    titles: added pages titles

--- src/routes/article/[slug]/+page.svelte
+++ src/routes/article/[slug]/+page.svelte
@@ -3,12 +3,12 @@
 	import { page } from '$app/state';
 	import { LucideLoaderPinwheel } from 'lucide-svelte';
 	import { onMount } from 'svelte';
-  import type { Component } from 'svelte';
+	import type { Component } from 'svelte';
 
 	let Content = $state<Component | null>(null);
 
 	onMount(async () => {
-		Content = await import(`$lib/articles/${languageTag()}/${page.params.slug}.svx`) as Component;
+		Content = (await import(`$lib/articles/${languageTag()}/${page.params.slug}.svx`)) as Component;
 	});
 </script>
 
