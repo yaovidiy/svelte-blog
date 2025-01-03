df1bb54e6b330bef01c6c8d1bce3bf0f094aaf5c
Yaroslav Ovdii <yaovdiy@gmail.com>
2025-01-03 17:05:45 +0200

    diagnostic: added diagnostic

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 	import { ModeWatcher } from 'mode-watcher';
 	import { Toaster } from '$lib/components/ui/sonner';
 	import Header from '$lib/components/app/header/Header.svelte';
 	import { i18n } from '$lib/i18n';
 	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
 	import '../app.css';
+
 	let { children } = $props();
+
+	injectSpeedInsights();
 </script>
 
 <ParaglideJS {i18n}>
